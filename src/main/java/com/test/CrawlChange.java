177e5366076ac18bac7c9858eec4a338b4e9086c
astro <1350617101@qq.com>
2018-11-26 17:44:45 +0800

    modify CrawlChange.java print ok

--- src/main/java/com/test/CrawlChange.java
+++ src/main/java/com/test/CrawlChange.java
@@ -5,57 +5,56 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.LinkedList;
-public class CrawlChange {
+import java.util.concurrent.CountDownLatch;
+public class CrawlChange{
     public static LinkedList<String> getAbsHref(){
         LinkedList<String> absHrefArr = new LinkedList();
         String basicPage = "https://beijing.zbj.com/wzkf/";
         String url;
         for (int i = 0; i < 100; i++){
-                if (i == 0) {
-                    url = basicPage + "e.html";
-                }
-                else {
-                    url = basicPage + "ek" + String.valueOf(i*60) + ".html";
-                }
+                url = basicPage + "ek" + String.valueOf(i*60) + ".html";
                 absHrefArr.offer(url);
         }
         return absHrefArr;
     }
-    public static class MyThread extends Thread {
+    public static class MyThread extends Thread{
         LinkedList<String>  subAbsHrefQueue;
         LinkedList<String>  absHref;
         ThreadGroup threadGroup;
-        public MyThread(LinkedList  subAbsHrefQueue, LinkedList  absHref,ThreadGroup threadGroup) {
+        public MyThread(LinkedList subAbsHrefQueue, LinkedList absHref, ThreadGroup threadGroup){
             this.subAbsHrefQueue = subAbsHrefQueue;
             this.absHref = absHref;
             this.threadGroup = threadGroup;
         }
         @Override
-        public void run() {
+        public void run(){
             Document document;
             Elements brandName;
             try {
-                while (absHref.peek() != null){
-                    if (subAbsHrefQueue.peek() == null)
-                    {
-                        subAbsHrefQueue.offer(absHref.poll());
-                    }
-                    else
-                    {
-                        document = Jsoup.connect(subAbsHrefQueue.poll()).get();
+                while (! absHref.isEmpty()){
+                    String next = subAbsHrefQueue.poll();
+                    if (next != null){
+                        document = Jsoup.connect(next).get();
                         brandName = document.select("span.shop-info-base-name.text-overflow");
-                        for (Element name : brandName) {
-                            System.out.println(getId() + " " + name.text() + "  "+ "pool中个数"+threadGroup.activeCount());
+                        for (Element name : brandName){
+                            System.out.println(getId() + "\t" + name.text());
                         }
                     }
+                    else {
+                        subAbsHrefQueue.offer(absHref.poll());
+                    }
                 }
-            } catch (IOException e) {
+            } catch (IOException e){
                 e.printStackTrace();
+            } finally {
+                countDownLatch.countDown();
             }
         }
     }
-    public static void main(String args[]) {
+    public static CountDownLatch countDownLatch;
+    public static void main(String args[]){
         Thread[] pool = new Thread[20];
+        countDownLatch = new CountDownLatch(20); // 初始化容量，
         LinkedList<String> absHref = getAbsHref();
         LinkedList<String> queue = new LinkedList<String>();
         ThreadGroup threadGroup = new ThreadGroup("test-group");
@@ -63,6 +62,13 @@ public class CrawlChange {
             pool[i] = new MyThread(queue,absHref,threadGroup);
             pool[i] = new Thread(threadGroup, pool[i]);
             pool[i].start();
+        }
+        try {
+            countDownLatch.await();   //等待子线程全部执行结束（等待CountDownLatch计数变为0）
+            System.out.println("ok");
+        } catch (InterruptedException e){
+                e.printStackTrace();
+                System.exit(0);
         }
     }
 }
